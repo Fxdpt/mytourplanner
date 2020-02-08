467c5b13b85f46f7bb55ba5d82c00f32d8efab39
Fxdpt <jaouen.jeremypro@gmail.com>
2020-02-08 08:13:18 +0100

    new: method clearInput

--- back/src/services/JsonHandler.ts
+++ back/src/services/JsonHandler.ts
@@ -1,5 +1,18 @@
+const sanitizeHtml = require('sanitize-html')
+
 export class JsonHandler {
 
+    static clearInput = (formData:object) => {
+        const clearInput:object = {}
+
+        for(const [key,value] of (<any>Object).entries(formData)){
+            if (typeof value === 'string') value.trim()
+            sanitizeHtml(value)
+            clearInput[key] = value
+        }
+
+        return clearInput
+    }
     // return a JSON formatted Response
     // TODO: handle status Code
     static JsonResponse = (success:boolean, message:string) => {
