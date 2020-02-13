1f09d2aa0e688cdeef1e218892db64073aaf24c8
Fxdpt <jaouen.jeremypro@gmail.com>
2020-02-13 19:37:29 +0100

    src: send credentials with axios and get token as response

--- front/src/components/organisms/SignIn.js
+++ front/src/components/organisms/SignIn.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { TextField, Button } from '@material-ui/core'
+import axios from 'axios'
 import './SignIn.css'
 
 function SignIn(props) {
+    const [credentials,setCredentials] = useState({
+        email:'',
+        password:''
+    })
+
+    const handleChange = (e) => {
+        const value = e.currentTarget.value
+        const name = e.currentTarget.name
+
+        setCredentials({...credentials, [name]:value})
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('toto')
+        
+        try{
+            axios.post('http://localhost:5000/login',credentials)
+                .then( res => {
+                    console.log(res)
+                })
+        }catch(err){
+            console.log(err.response)
+        }
+        
     }
 
     return (
         <div className="form-container">
             <form>
-                <TextField color="secondary" className="formInput" aria-label="signin-email" label="email" type="email" name="username" />
-                <TextField color="secondary" className="formInput" aria-label="signin-password" label="mot de passe" type="password" name="password" />
+                <TextField color="secondary" onChange={handleChange} value={credentials.username} className="formInput" aria-label="signin-email" label="email" type="email" name="email" />
+                <TextField color="secondary" onChange={handleChange} value={credentials.password} className="formInput" aria-label="signin-password" label="mot de passe" type="password" name="password" />
                 <Button id="signin-form-button" type="submit" variant="contained" color="primary" onClick={handleSubmit}>
                     Envoyer
                 </Button>
